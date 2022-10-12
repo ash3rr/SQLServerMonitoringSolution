a9f43d9c71a7d4d0e1bbd6dfbd7aaeab776e0676
Asher Rankin <asher.rankin@bbraun.com>
2022-10-12 14:42:25 +0300

    install script

---
+++ install.ps1
@@ -0,0 +1,19 @@
+﻿IF (!(Get-Module -Name sqlps))
+    {
+        Write-Host 'Loading SQLPS Module' -ForegroundColor DarkYellow
+        Push-Location
+        Import-Module sqlps -DisableNameChecking
+        Pop-Location
+    }
+  
+  
+$localScriptRoot = "D:\install\SQLMonSol"
+$Server = "localhost"
+$scripts = Get-ChildItem $localScriptRoot | Where-Object {$_.Extension -eq ".sql"}
+  
+foreach ($s in $scripts)
+    {
+        Write-Host "Running Script : " $s.Name -BackgroundColor DarkGreen -ForegroundColor White
+        $script = $s.FullName
+        Invoke-Sqlcmd -ServerInstance $Server -InputFile $script
+    }
